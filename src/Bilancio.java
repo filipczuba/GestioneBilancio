4266bec66156313328de2feb9abd4f3bfc577f42
filipczuba <filipczubaa@gmail.com>
2023-02-18 17:55:25 +0100

    Changed the Main class to Bilancio

--- src/Bilancio.java
+++ src/Bilancio.java
@@ -3,7 +3,7 @@ import View.MainView;
 /**
  * Classe Main, contiene il metodo main e istanzia una nuova istanza di {@link MainView}.
  */
-public class Main {
+public class Bilancio {
 
     /**
      * Metodo main.
